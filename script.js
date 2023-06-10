ee1daa3ce7c57d4c97f39125ac56a097c8d3321e
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-10 20:08:44 +0300

    find 7

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Good Luck 💪🏾
+const numbers = [1, 2, 3, 4, 5,5,6,7];
+
+const foundseven = numbers.find(element => element == 7);
+if (foundseven ==7){
+    console.log("found")
+}else{
+    console.log("there is no seven");
+}
